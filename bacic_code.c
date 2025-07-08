3ab82ab344409737e46f86b0ecaf76fb5223ff51
riot73 <andkohee@gmail.com>
2025-07-08 22:35:42 +0600

    Create bacic_code.c

---
+++ bacic_code.c
@@ -0,0 +1,66 @@
+//using the assert method
+#include<stdio.h>
+#include<assert.h>
+#define NDEBUG
+int main(void){
+    double number = -10.2 ;
+    assert(number > -10.3) ;
+    printf("Done it is !~") ;
+    return 0 ;
+}
+--if u use the NDEBUG method , then the compiler will avoid all kind of assert functions and logic 
+--using assert for debugging is great but its low the performance ....for production after debugging use NDEBUG //
+
+//Another simple example of assert and sqrt() 
+#include<stdio.h>
+#include<math.h>
+#include<assert.h>
+int main(void){
+    double number = -1.0021 ;
+    assert(number >= 0) ;
+    printf("The squire of number is :%.4f\n" , sqrt(number)) ;
+}
+--check it before compiling because sqrt(negetive) is not valid ...first check that this is positive or not ! 
+
+//malloc() only work for void pointer so first convert into int if need or any data types whatever 
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+    int *ptr = (int *)malloc(sizeof(int)) ;
+    if(ptr == NULL){
+        perror("Failed .....") ;
+        return -1 ;
+    }
+    *ptr = 10 ;
+    printf("Allocated number is :%d\n" , *ptr) ;
+    printf("Addr :%p\n" , ptr) ;
+    free(ptr) ;
+}
+
+//double pointer 
+#include<stdio.h>
+int main(void){
+    int number = 10 ;
+    int *ptr1 = &number ;
+    int **ptr2 = &ptr1 ;
+    printf("Number is :%d\n" , number) ;
+    printf("ptr1 is :%d\n" , *ptr1) ;
+    printf("ptr 2 is :%d\n" , **ptr2) ;
+    return 0 ;
+}
+//A simple example of double ptr and with malloc() 
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+    int *ptr ;
+    int **ptr2 ;
+    ptr = (int *)malloc(sizeof(int)) ;
+    if(ptr == NULL){
+        perror("Failed to do it !") ;
+        return -1 ;
+    }
+    *ptr = 666 ;
+    ptr2 = &ptr ;
+    printf("ptr 1 is :%d\n" , *ptr) ;
+    printf("ptr 2 is :%d\n" , **ptr2) ;
+}
